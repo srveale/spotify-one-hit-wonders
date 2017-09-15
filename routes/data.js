9ac78687e65e60d93407bf18bbf559721bd91b93
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-15 15:34:23 -0600

    changed precision

--- routes/data.js
+++ routes/data.js
@@ -1,7 +1,7 @@
 /* Several steps before getting the actual data
- 1. Get access token from https://accounts.spotify.com/api/token
-2. Get artist id from https://api.spotify.com/v1/search
-3. Get artist tracks from https://api.spotify.com/v1/artists/{id}/top-tracks
+1. Get access token from https://accounts.spotify.com/api/token
+	2. Get artist id from https://api.spotify.com/v1/search
+		3. Get artist tracks from https://api.spotify.com/v1/artists/{id}/top-tracks
 
 Question: How to handle the access token? Can't be for every search - I
  think it won't let me request that many. Where to store it? Later: How to refresh it?
@@ -31,55 +31,50 @@ const tokenOptions = {
 	method: "POST"
 }
 
-/* GET users listing. */
-router.get('/artist', function(req, res, next) {
+// Get an artists track popularity data from the artist name
+router.get('/:artist', function(req, res, next) {
 	// Check if a fresh token exists
 	const saved_access_token = require('../config.js').access_token;
+	const encodedArtist = encodeURI(req.params.artist)
+	console.log("encodedArtist", encodedArtist);
 
+	// Get access token
 	if (!saved_access_token) {
 		request.post(tokenOptions, (tokenError, tokenRes, tokenBody) => {
-			console.log('typof tokenBody', typeof tokenBody)
+			const parsedTokenBody = JSON.parse(tokenBody);
 			console.log('tokenBody', tokenBody)
-			console.log('access_token', tokenBody['access_token'])
-			const authOptions = {
-				url: 'https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF/top-tracks?country=CA',
+
+			// Get Artist Id
+			const searchOptions = {
+				url: `https://api.spotify.com/v1/search?q=${encodedArtist}&type=artist`,
 				headers: {
-					'Authorization': 'Bearer ' + 'BQDCDJUjbncVCnQg2X1T7KhUbJJdbfZmxHwkunTlmx_drtPX3IuhCrr5lDQtXiUM60zNXnol7BCGiX3LRoSgtA'
+					'Authorization': 'Bearer ' + parsedTokenBody['access_token']
 				},
 				json: true,
 				method: "POST"
-			};
-			request.get(authOptions, (tracksError, tracksRes, tracksBody)=> {
-				// Process track data and send it with response
-				console.log('second body', tracksBody)
-				const processedData = processTracks(tracksBody.tracks);
-				res.send(processedData)
+			}
+			request.get(searchOptions, (searchError, searchRes, searchBody) => {
+				const artistId = searchBody.artists.items[0].id;
+
+				// Get Track Data
+				const trackOptions = {
+					url: `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=CA`,
+					headers: {
+						'Authorization': 'Bearer ' + parsedTokenBody['access_token']
+					},
+					json: true,
+					method: "POST"
+				};
+				request.get(trackOptions, (tracksError, tracksRes, tracksBody)=> {
+					// Process track data and send it with response
+					const processedData = processTracks(tracksBody.tracks);
+					res.send(processedData)
+				})
+				
 			})
-		})
-	} else {
-		// Do the same thing but with the saved token
-		const authOptions = {
-			url: 'https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF/top-tracks?country=CA',
-			headers: {
-				'Authorization': 'Bearer ' + saved_access_token //bodyFirst.access_token
-			},
-			json: true,
-			method: "POST"
-		};
-		request.get(authOptions, (err, res, body)=> {
-			console.log('second body', body)
+
 		})
 	}
-
-
-	// res.json([{
-	// 	id: 1,
-	// 	username: "samsepi0l"
-	// }, {
-	// 	id: 2,
-	// 	username: "D0loresH4ze"
-	// }]);
-
 });
 
 module.exports = router;
--- scripts/process-tracks.js
+++ scripts/process-tracks.js
@@ -1,5 +1,13 @@
+const regression = require('regression');
+
 const processTracks = function (tracks) {
-	return tracks.map(track => track.popularity)
+	const popularities = tracks.map(track => track.popularity).sort().reverse();
+	console.log("popularities", popularities);
+	const fitParams = regression.exponential(popularities.map((pop, i) => [i, pop]), {precision: 3});
+	return {
+		popularities,
+		fitParams,
+	}
 }
 
 module.exports = processTracks;
