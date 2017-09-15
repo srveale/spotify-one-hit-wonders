82a81c6dbd316924aa8cfa4ff27949f991191f70
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-15 10:34:50 -0600

    added access token request to artist endpoint

--- routes/data.js
+++ routes/data.js
@@ -6,27 +6,74 @@
 // Question: How to handle the access token? Can't be for every search - I
 // think it won't let me request that many. Where to store it? Later: How to refresh it?
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const request = require('request');
+const processTracks = require('../scripts/process-tracks'); 
+
+const { redirect_uri, client_secret, client_id } = require('../config');
+
+const payload = client_id + ":" + client_secret;
+const encodedPayload = new Buffer(payload).toString("base64");
+
+const tokenOptions = {
+	url: 'https://accounts.spotify.com/api/token',
+	form: {
+		grant_type: 'client_credentials'
+	},
+	headers: {
+		"Authorization": 'Basic ' + encodedPayload
+	},
+	method: "POST"
+}
 
 /* GET users listing. */
 router.get('/artist', function(req, res, next) {
-	// Expects a keyword on req.body, eg "Muse"
-	// Calls the api and gets all track data
-	// runs fitting function
-	// returns tracking data and fit data, including OHW param
-	res.json([{
-		id: 1,
-		username: "samsepi0l"
-	}, {
-		id: 2,
-		username: "D0loresH4ze"
-	}]);
+	// Check if a fresh token exists
+	const saved_access_token = require('../config.js').access_token;
 
+	if (!saved_access_token) {
+		request.post(tokenOptions, (errFirst, resFirst, bodyFirst) => {
+			console.log('first body', bodyFirst.access_token)
+			const authOptions = {
+				url: 'https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF/top-tracks?country=CA',
+				headers: {
+					'Authorization': 'Bearer ' + bodyFirst.access_token
+				},
+				json: true,
+				method: "POST"
+			};
+			request.get(authOptions, (err, res, body)=> {
+				// Process track data and send it with response
+				console.log('second body', body)
+				const processedData = processTracks(body.tracks);
+				res.json(processedData)
+			})
+		})
+	} else {
+		// Do the same thing but with the saved token
+		const authOptions = {
+			url: 'https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF/top-tracks?country=CA',
+			headers: {
+				'Authorization': 'Bearer ' + saved_access_token //bodyFirst.access_token
+			},
+			json: true,
+			method: "POST"
+		};
+		request.get(authOptions, (err, res, body)=> {
+			console.log('second body', body)
+		})
+	}
+
+
+	// res.json([{
+	// 	id: 1,
+	// 	username: "samsepi0l"
+	// }, {
+	// 	id: 2,
+	// 	username: "D0loresH4ze"
+	// }]);
 
 });
 
 module.exports = router;
-
-
---
+++ scripts/process-tracks.js
@@ -0,0 +1,3 @@
+const processTracks = function (tracks) {
+	return tracks.map(track => track.popularity)
+}
