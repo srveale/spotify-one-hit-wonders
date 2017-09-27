ac5feaec45e31cabfbf923b13e29d564640582d7
Scott Veale <scottveale@Scotts-MBP.hitronhub.home>
2017-09-27 13:32:26 -0600

    enabled CORS

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ var data = require('./routes/data');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Enable CORS - https://github.com/facebookincubator/create-react-app/issues/586
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
