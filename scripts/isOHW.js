89fafb60c7a31b9022185dff82548b6b7823e55f
scott <srveale42@gmail.com>
2017-09-30 13:38:46 -0600

    fixing cors

--- scripts/isOHW.js
+++ scripts/isOHW.js
@@ -1,8 +1,30 @@
 const isOHW = (trackData, fitParams) => {
   const hasHit = trackData.map(track=> track.popularity).some(pop => pop > 50);
-  const onlyOneHit = Math.abs(fitParams.equation[1]) * 1000 > 100;
+  const ohwFactor = Math.abs(fitParams.equation[1]) * 1000;
 
-  return hasHit && onlyOneHit;
-}
+  const onlyOneHit = ohwFactor > 100;
+
+  const ohwBool = hasHit && onlyOneHit;
+  switch (true) {
+    case (ohwFactor < 20):
+      const ohwString = ohwStrings.superLow;
+    case (ohwFactor < 50):
+      const ohwString = ohwStrings.low;
+    case (ohwFactor < 75):
+      const ohwString = ohwStrings.medium;
+    case (ohwFactor < 100):
+      const ohwString = ohwStrings.high;
+    default:
+      const ohwString = ohwStrings.superHigh;
+  }
+  return { ohwBool, ohwString };
 
 module.exports = isOHW
+
+const ohwStrings = {
+  "superLow": "Not even close!",
+  "low": "Not really",
+  "medium": "Middle of the pack",
+  "high": "Getting up there",
+  "superHigh": "Certified One-Hit-Wonder"
+}
